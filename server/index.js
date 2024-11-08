9783a84d7c410a3ad966cc3cbf4baeb2506c2b2c
foferys <foferys@gmail.com>
2024-11-08 16:47:29 +0100

    aggiunta commenti

--- server/index.js
+++ server/index.js
@@ -1,30 +1,35 @@
 // installare tutte le dipendenze con nmp (express, mongoose, cors)
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require('express'); //-> gestisce il server web e le rotte.
+const mongoose = require('mongoose');  //-> permette di interagire con MongoDB usando un'interfaccia orientata a oggetti.
+const cors = require('cors'); // abilita le richieste cross-origin, permettendo al frontend di comunicare con il backend da domini diversi.
 const userModel = require('./models/Users.js')
 
-const app = express()
-app.use(cors())
-app.use(express.json());
-// questo fa si che quando passiamo dati da frontend e back sarà convertito in json
+//--> COSA FA? -> usare Express e Mongoose per connettersi a un database MongoDB e fornire dati tramite un'API:
 
-// connessione al database tramite server
+
+const app = express() //->//express(): crea un'istanza dell'applicazione.
+app.use(cors()) // -> abilita il CORS, così il server può accettare richieste da domini diversi.
+app.use(express.json()); // -> abilita il parsing JSON automatico per le richieste, utile per gestire il corpo delle richieste JSON.
+
+// connessione al database tramite server:
+/*  mongoose.connect(...): stabilisce la connessione con il database MongoDB.
+    "mongodb://127.0.0.1:27017/corsomongocompass" è l'URL locale per MongoDB; 127.0.0.1 è l'indirizzo IP locale e corsomongocompass è il nome del database. */
 mongoose.connect("mongodb://127.0.0.1:27017/corsomongocompass");
 
+//Endpoint GET per Recuperare i Dati
 // va a prendere il model "Users.js" nella cartella model che abbiamo importato come userModel sopra
-app.get('/', (req, res) => {
-    userModel.find() //->questo metodo recupera tutti i records dal database
+app.get('/', (req, res) => { // ->app.get('/'): crea un endpoint GET per la rotta radice (/).
+    userModel.find() //->usa Mongoose per recuperare tutti i documenti (records) della collezione users nel database.
     // convertiamo in json la risposta
-    .then(users => res.json(users))
+    .then(users => res.json(users)) //-> se userModel.find() ha successo, la lista degli utenti è inviata come JSON nella risposta.
     .catch(err => res.json(err))
 
 });
 
-// funzione per avviare il server con messaggio 
+//Avvio del Server
 // installare nodemon per averlo nelle dipendenze e poter far partire il server
-app.listen(3001, () => {
+app.listen(3001, () => { // -> app.listen(3001, ...): avvia il server su localhost alla porta 3001, e stampa un messaggio di conferma in console.
     console.log("server attivo")
-})
+}) 
 
 
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,12 +1,14 @@
+//questo file Users.js definisce il modello per la collezione users nel database richiesta nel file index.js
+
 const mongoose = require("mongoose")
 
-// creaiamo il riferimento della nostra tabella/Collection con il nome e il tipo che abbiamo sul
-// database
+// creaiamo il riferimento della nostra tabella/Collection con il nome e il tipo che abbiamo sul database
+//userSchema: è lo schema di Mongoose per la collezione users, che definisce i campi nome e email come stringhe.
 const userSchema = new mongoose.Schema({
     nome: String,
     email: String,
 })
 
-// creaiamo il model che prende 2 parametri -> il nome della tabella e lo schema creato
+// userModel: crea un modello Mongoose basato sullo schema userSchema e mappa il modello alla collezione users nel database.
 const userModel = new mongoose.model("users", userSchema)
-module.exports = userModel;
+module.exports = userModel; // ->  esporta il modello userModel per permetterne l'uso in altri file, come il file principale del server(index.js)
